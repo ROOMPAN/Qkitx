e8ffaf38cb966bac4c63717227befa7a07526004
liupp <liupp@123>
2017-10-23 14:31:13 +0800

    修改

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -306,3 +306,15 @@ public static final int *;
 #腾讯 X5内核
 -dontwarn com.just.agentwebX5.**
 -keep class com.just.agentwebX5.** { *; }
+
+#阿里热修复
+#基线包使用，生成mapping.txt
+-printmapping mapping.txt
+#生成的mapping.txt在app/buidl/outputs/mapping/release路径下，移动到/app路径下
+#修复后的项目使用，保证混淆结果一致
+#-applymapping mapping.txt
+#hotfix
+-keep class com.taobao.sophix.**{*;}
+-keep class com.ta.utdid2.device.**{*;}
+    #防止inline
+-dontoptimize
--- app/src/main/java/com/yanhui/qktx/MyApplication.java
+++ app/src/main/java/com/yanhui/qktx/MyApplication.java
@@ -14,6 +14,9 @@ import com.bumptech.glide.request.target.ViewTarget;
 import com.google.gson.Gson;
 import com.lzy.imagepicker.ImagePicker;
 import com.lzy.imagepicker.view.CropImageView;
+import com.taobao.sophix.PatchStatus;
+import com.taobao.sophix.SophixManager;
+import com.taobao.sophix.listener.PatchLoadStatusListener;
 import com.tencent.smtt.sdk.QbSdk;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.analytics.MobclickAgent.UMAnalyticsConfig;
@@ -57,6 +60,13 @@ public class MyApplication extends Application {
     private String channel;
 
     @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        //加载热修复
+        initialize();
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
         //对全局属性赋值
@@ -70,7 +80,8 @@ public class MyApplication extends Application {
         InitUmMobclick();
         initPushAgent();
         initImagePicker();
-
+// 热修复 queryAndLoadNewPatch不可放在attachBaseContext 中，否则无网络权限，建议放在后面任意时刻，如onCreate中
+        SophixManager.getInstance().queryAndLoadNewPatch();
     }
 
     /**
@@ -262,5 +273,28 @@ public class MyApplication extends Application {
             }
         });
 
+    }
+
+    public void initialize() {
+        // initialize最好放在attachBaseContext最前面，初始化直接在Application类里面，切勿封装到其他类
+        SophixManager.getInstance().setContext(this)
+                .setAppVersion("1.0")
+                .setAesKey(null)
+                .setEnableDebug(true)
+                .setPatchLoadStatusStub(new PatchLoadStatusListener() {
+                    @Override
+                    public void onLoad(final int mode, final int code, final String info, final int handlePatchVersion) {
+                        // 补丁加载回调通知
+                        if (code == PatchStatus.CODE_LOAD_SUCCESS) {
+                            // 表明补丁加载成功
+                        } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
+                            // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
+                            // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
+                            android.os.Process.killProcess(android.os.Process.myPid());
+                        } else {
+                            // 其它错误信息, 查看PatchStatus类说明
+                        }
+                    }
+                }).initialize();
     }
 }
